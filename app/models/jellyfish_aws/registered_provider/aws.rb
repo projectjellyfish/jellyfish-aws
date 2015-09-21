f6ea7b20175b301f9d54e01bf841ad3ecc8ef713
Michael Stack <michael.stack@gmail.com>
2015-09-21 10:42:24 -0500

    Namespace all classes
    Collisions between extensions could occur if things are not namespaced.

---
+++ app/models/jellyfish_aws/registered_provider/aws.rb
@@ -0,0 +1,35 @@
+module JellyfishAws
+  class RegisteredProvider < ActiveRecord::Base
+    class Aws < ::RegisteredProvider
+      def self.load_registered_providers
+        return unless super
+
+        transaction do
+          [
+            set('AWS', '1072ddc9-e04f-4d43-929b-8866ce7e6d3a')
+          ].each { |s| create! s.merge!(type: 'JellyfishAws::RegisteredProvider::Aws') }
+        end
+      end
+
+      def provider_class
+        'JellyfishAws::Provider::Aws'.constantize
+      end
+
+      def description
+        'Amazon Web Services'
+      end
+
+      def tags
+        ['aws']
+      end
+
+      def questions
+        [
+          { name: :access_id, value_type: :string, field: :text, label: 'Access ID', required: true },
+          { name: :secret_key, value_type: :password, field: :password, label: 'Secret Key', required: :if_new },
+          { name: :region, value_type: :string, field: :aws_regions, required: true }
+        ]
+      end
+    end
+  end
+end
---
+++ db/migrate/20150917174153_namespace_classes.rb
@@ -0,0 +1,10 @@
+class NamespaceClasses < ActiveRecord::Migration
+  def up
+    Provider.where(type: 'Provider::Aws').update_all(type: 'JellyfishAws::Provider::Aws')
+    Service..where(type: 'Service::Ec2').update_all(type: 'JellyfishAws::Service::Ec2')
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
