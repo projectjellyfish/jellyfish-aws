a329efc773e85ca708a93555c93dc7bc6dbd2cba
Michael Stack <michael.stack@gmail.com>
2015-09-21 10:52:17 -0500

    Correct double period issue

--- db/migrate/20150917174153_namespace_classes.rb
+++ db/migrate/20150917174153_namespace_classes.rb
@@ -1,7 +1,7 @@
 class NamespaceClasses < ActiveRecord::Migration
   def up
     Provider.where(type: 'Provider::Aws').update_all(type: 'JellyfishAws::Provider::Aws')
-    Service..where(type: 'Service::Ec2').update_all(type: 'JellyfishAws::Service::Ec2')
+    Service.where(type: 'Service::Ec2').update_all(type: 'JellyfishAws::Service::Ec2')
   end
 
   def down
