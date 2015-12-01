f1224f4aa065bf049393b9e12f5f125862523079
mafernando <fernando_michael@bah.com>
2015-11-30 20:40:57 -0600

    Adds S3 product and service

---
+++ app/models/jellyfish_aws/product_type/s3.rb
@@ -0,0 +1,39 @@
+module JellyfishAws
+  module ProductType
+    class S3 < ::ProductType
+      def self.load_product_types
+        return unless super
+
+        transaction do
+          [
+            set('S3 Instance', '11ca0e0e-617d-45f3-b10a-acf42d5e6ecc', provider_type: 'JellyfishAws::Provider::Aws')
+          ].each do |s|
+            create! s.merge!(type: 'JellyfishAws::ProductType::S3')
+          end
+        end
+      end
+
+      def description
+        'Amazon Web Services Scalable Cloud Storage Instance'
+      end
+
+      def tags
+        ['s3', 'storage', 'cdn']
+      end
+
+      def product_questions
+        [
+        ]
+      end
+
+      def order_questions
+        [
+        ]
+      end
+
+      def service_class
+        'JellyfishAws::Service::S3'.constantize
+      end
+    end
+  end
+end
---
+++ public/extensions/aws/states/services/details/aws/s3/s3.state.js
@@ -0,0 +1,60 @@
+(function() {
+  'use strict';
+
+  angular.module('app.states')
+    .run(appRun);
+
+  /** @ngInject */
+  function appRun(StateOverride) {
+    StateOverride.override('services.details', function(service) {
+      if ('JellyfishAws::Service::S3' == service.type) {
+        return {
+          templateUrl: 'extensions/aws/states/services/details/aws/s3/s3.html',
+          controller: StateController
+        };
+      }
+    })
+  }
+
+  /** @ngInject */
+  function StateController(service, AwsData) {
+    var vm = this;
+
+    vm.title = '';
+    vm.service = service;
+    vm.getServiceOutput = getServiceOutput;
+
+    vm.activate = activate;
+
+    vm.deprovision = deprovision;
+
+    activate();
+
+    function activate() {
+    }
+
+    function handleResponse(response) {
+      console.log(response);
+      vm.response = response;
+    }
+
+    function handleError(response) {
+      console.log(response);
+      vm.response = response;
+    }
+
+    function deprovision(){
+      vm.response = null;
+      AwsData['deprovision'](vm.service.provider.id, vm.service.id).then(handleResponse, handleError);
+    }
+
+    function getServiceOutput(service_output_name){
+      var outputs = vm.service.service_outputs.filter(function(elt, idx){ return elt.name == service_output_name });
+      if(outputs.length > 0){
+        return outputs[0];
+      }else{
+        return null;
+      }
+    }
+  }
+})();
