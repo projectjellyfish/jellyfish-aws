41c6aace75699d12758bbb01be00069bf5206795
mafernando <fernando_michael@bah.com>
2015-12-07 12:41:10 -0600

    Add RDS provision & deprovision implementation

--- app/models/jellyfish_aws/service/rds.rb
+++ app/models/jellyfish_aws/service/rds.rb
@@ -8,9 +8,17 @@ module JellyfishAws
 
       def deprovision
         handle_errors do
-          # client.delete_bucket(get_output('instance_id').value)
-          # snapshot_id = skip_final_snap_shot ? '' : snapshot
-          # connection.delete_db_instance(identifier, snapshot_id, skip_final_snap_shot)
+          # GET HANDLE OF SERVER INSTANCE TO BE DEPROVISIONED
+          identifier = get_output('instance_id').value
+
+          # DO NOT MAKE A BACKUP SNAPSHOT
+          snapshot_id = ''
+          skip_final_snap_shot = true
+
+          # SEND THE SIGNAL TO SHUTDOWN AND TERMINATE THE INSTANCE
+          client.delete_db_instance(identifier, snapshot_id, skip_final_snap_shot)
+
+          # UPDATE STATUS
           update_status(::Service.defined_enums['status']['terminated'], 'terminated')
         end
       end
@@ -18,18 +26,34 @@ module JellyfishAws
       def provision
         db = nil
         handle_errors do
-          # instance_id = "id-#{SecureRandom.hex(10)}"
-          # details = {
-          #   flavor_id: self.product.answers.find { |x| x.name == 'flavor_id' }.value,
-          #   engine: self.product.answers.find { |x| x.name == 'engine' }.value,
-          #   allocated_storage: self.product.answers.find { |x| x.name == 'allocated_storage' }.value,
-          #   master_username: self.product.answers.find { |x| x.name == 'master_username' }.value,
-          #   password: self.product.answers.find { |x| x.name == 'password' }.value
-          # }
-          # db = connection.create_db_instance(instance_id, details)
-          # binding.pry
-          # @order_item.payload_response['raw']['data']['body']['CreateDBInstanceResult']['DBInstance']['DBInstanceIdentifier']
-          # save_outputs({ key: storage.key }, [ [ 'instance_id', :key ] ], ValueTypes::TYPES[:string]) if defined? storage
+          # UPDATE PRODUCT DETAILS
+          instance_id = "id-#{SecureRandom.hex(10)}"
+          details = {
+            'Engine' => self.product.answers.find { |x| x.name == 'engine' }.value,
+            'EngineVersion' => self.product.answers.find { |x| x.name == 'version' }.value,
+            'DBInstanceClass'=> self.product.answers.find { |x| x.name == 'flavor_id' }.value,
+            'AllocatedStorage' => self.product.answers.find { |x| x.name == 'allocated_storage' }.value,
+            'MasterUsername' => self.product.answers.find { |x| x.name == 'master_username' }.value,
+            'MasterUserPassword' => self.product.answers.find { |x| x.name == 'master_password' }.value
+          }
+
+          # CREATE THE AWS DB INSTANCE
+          # TODO: WAIT FOR ACTUAL AWS PROVISIONING TO COMPLETE BEFORE SETTING STATUS TO RUNNING - POLL?
+          # ISSUE: NoMethodError: undefined method `wait_for' for #<Excon::Response:0x007fdb08dda488>
+          db = client.create_db_instance(instance_id, details)
+
+          # SAVE DB INSTANCE DETAILS
+          save_outputs(db.body['CreateDBInstanceResult']['DBInstance'], [ ['instance_id', 'DBInstanceIdentifier'] ], ValueTypes::TYPES[:string]) if defined? db.body['CreateDBInstanceResult']['DBInstance']
+
+          # SAVE DB PRODUCT DETAILS
+          save_outputs(details, [ ['engine', 'Engine'],
+                                  ['version', 'EngineVersion'],
+                                  ['flavor_id', 'DBInstanceClass'],
+                                  ['allocated_storage', 'AllocatedStorage'],
+                                  ['master_username','MasterUsername'],
+                                  ['master_password','MasterUserPassword']], ValueTypes::TYPES[:string]) if defined? details
+
+          # UPDATE STATUS
           update_status(::Service.defined_enums['status']['running'], 'running')
         end
       end
